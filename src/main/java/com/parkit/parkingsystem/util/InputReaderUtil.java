9cffb79ab81c2b4664738bc2f13b4d1c474f24a2
Johnny Makhoul <johnny.makhoul@isae.edu.lb>
2024-02-28 15:45:17 +0200

    Specify UTF-8 encoding in InputReaderUtil

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 
 public class InputReaderUtil {
 
-    private static Scanner scan = new Scanner(System.in);
+    private static Scanner scan = new Scanner(System.in, "UTF-8");
+
     private static final Logger logger = LogManager.getLogger("InputReaderUtil");
 
     public int readSelection() {
